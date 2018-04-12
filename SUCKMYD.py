087af69e96cea47672f47e0317d32e40a4613b15
FREYAAAAA <37949888+FREYAAAAA@users.noreply.github.com>
2018-04-12 12:42:52 +0800

    Merge branch 'master' of https://github.com/Gashima3/Testing

--- SUCKMYD.py
+++ SUCKMYD.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
+<<<<<<< HEAD
     return "Hello World!say whatttttt"
+=======
+    return "Hello World!Investigating!YOYO"
+>>>>>>> a8e2b7c2e00b138ae3abfbaec6d56fe25196e168
 
 if __name__=="__main__":
         app.run()
